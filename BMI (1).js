819fd10228682ee7ec09341dde89fa9c467a42d2
prekshita <prekshitavpatil@gmail.com>
2021-08-07 13:57:46 +0530

    initial

---
+++ BMI (1).js
@@ -0,0 +1,115 @@
+// function calc()
+// {
+//     var n1 = parseFloat(document.getElementById("input1").value)
+//     var n2 = parseFloat(document.getElementById("input2").value)
+//     // document.getElementById('Output').value = n1+n2
+//     document.getElementById('text').value = n1+n2
+//     document.getElementById('div').innerHTML += '<b>'+(n1+n2)+'</b>'
+// }
+
+
+
+// var weight = parseFloat(prompt("Enter Weight in kg"))
+// var height = parseFloat(prompt("Enter Height in meters"))
+// var bodymassindex =   weight/(Math.pow(height,2))
+// console.log(weight,height)
+// console.log(bodymassindex)
+
+
+
+
+
+window.onload = () => {
+    let button = document.querySelector("#check");
+    let refresh1 = document.querySelector("#refresh");
+    // Function for calculating BMI
+    button.addEventListener("click", check);
+    refresh1.addEventListener("refresh", refreshPage);
+};
+
+function refreshPage(){
+    window.location.reload();
+} 
+
+function check()
+{
+    var weight = parseInt(document.getElementById("weight").value)
+    let weightoption = document.getElementById("weightunits")
+    var idx = weightoption.selectedIndex;
+    var weightopt = weightoption.options[idx].value;
+    if (weightopt == "Grams") {
+        weight = weight/1000
+    }
+    else if(weightopt == "lbs"){
+        weight = (weight*0.45359237)
+    }
+    else {
+        // pass
+    }
+    console.log(weight, weightopt)
+    
+    var height = parseInt(document.getElementById("height").value)
+    let heightoption = document.getElementById("heightunits")
+    var idx = heightoption.selectedIndex;
+    var heightopt = heightoption.options[idx].value;
+
+    if (heightopt == "centi-meters") {
+        height = height/100
+    }
+    else if (heightopt == "inches") {
+        height = height/39.370
+        height = height*height
+    }
+    else {
+        // pass
+    }
+
+    console.log(height, heightopt)
+    
+    document.getElementById('div').innerHTML +=(weight / (height*height))
+    let bodymassindex = weight / (height*height)
+    console.log(bodymassindex)
+
+    if (bodymassindex <= 18.5)
+    {
+        // document.write("Under Weight")
+        console.log("Under Weight")
+        document.getElementById('div').innerHTML += " is BMI so"+" Under Weight"
+    }
+    else if(bodymassindex >= 18.5 && bodymassindex <= 24.9)
+    {
+        // document.write("Normal Weight")
+        console.log("Normal Weight")
+        document.getElementById('div').innerHTML += " is BMI so"+" Normal Weight"
+    }
+    else if(bodymassindex >= 25.0 && bodymassindex <=29.9 )
+    {   
+        // document.write("Over Weight")
+        console.log("Over Weight")
+        document.getElementById('div').innerHTML += " is BMI so"+" Over Weight"
+    }
+    else
+    {
+        // document.write("Obese ")
+        console.log("Obese ")
+        document.getElementById('div').innerHTML += " is BMI so"+" Obese "
+        if(bodymassindex >= 30.0 && bodymassindex <=34.9)
+        {
+            // document.write("Class 1 ")
+            console.log("Class 1")
+            document.getElementById('div').innerHTML += "and Class 1"
+        }
+        else if(bodymassindex >= 35.0 && bodymassindex <=39.9 )
+        {
+            // document.write("Class 2")
+            console.log("Class 2")
+            document.getElementById('div').innerHTML += "and Class 2"
+        }
+        else
+        {
+            // document.write("Class 3")
+            console.log("Class 3")
+            document.getElementById('div').innerHTML += "and Class 3"+'</b>'
+        }
+    }
+}
